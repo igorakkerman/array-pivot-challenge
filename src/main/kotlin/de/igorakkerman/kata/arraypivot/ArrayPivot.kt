4d9575bd56b493ef55b13135a11b200b3f23f411
Igor Akkerman <codingchallenge@igorakkerman.de>
2019-05-17 13:38:32 +0200

    Spy optimized

--- src/main/kotlin/de/igorakkerman/kata/arraypivot/ArrayPivot.kt
+++ src/main/kotlin/de/igorakkerman/kata/arraypivot/ArrayPivot.kt
@@ -1,4 +1,25 @@
 package de.igorakkerman.kata.arraypivot
 
-class ArrayPivot {
+open class ArrayPivot {
+
+    fun solution(a: IntArray, n: Int): IntArray {
+        if (a.size <= 1)
+            return a
+
+        repeat(n) {
+            val last = a[a.size - 1]
+            (a.size - 1 downTo 1).forEach {
+                set(a, it, a[it - 1])
+            }
+            set(a, 0, last)
+        }
+
+        return a
+    }
+
+    open fun set(a: IntArray, index: Int, value: Int) {
+        a[index] = value
+    }
 }
+
+
--- src/test/kotlin/de/igorakkerman/kata/arraypivot/ArrayPivotTest.kt
+++ src/test/kotlin/de/igorakkerman/kata/arraypivot/ArrayPivotTest.kt
@@ -1,11 +1,109 @@
 package de.igorakkerman.kata.arraypivot
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class ArrayPivotTest {
-    @Test
-    internal fun empty() {
-        ArrayPivot()
+
+    class ArrayPivotSpy : ArrayPivot() {
+
+        var shiftsCount = 0
+
+        override fun set(a: IntArray, index: Int, value: Int) {
+            shiftsCount ++
+            super.set(a, index, value)
+        }
     }
+
+    private lateinit var arrayPivot: ArrayPivotSpy
+
+    @BeforeEach
+    internal fun setUp() {
+        arrayPivot = ArrayPivotSpy()
+    }
+
+    @Test
+    internal fun `empty 0`() {
+        assertThat(arrayPivot.solution(intArrayOf(), 0))
+                .isEmpty()
+    }
+
+    @Test
+    internal fun `empty 1`() {
+        assertThat(arrayPivot.solution(intArrayOf(), 0))
+                .isEmpty()
+    }
+
+    @Test
+    internal fun `empty someNumber`() {
+        assertThat(arrayPivot.solution(intArrayOf(), 43))
+                .isEmpty()
+    }
+
+    @Test
+    internal fun `1 0`() {
+        assertThat(arrayPivot.solution(intArrayOf(1), 0))
+                .containsExactly(1)
+    }
+
+    @Test
+    internal fun `1 1`() {
+        assertThat(arrayPivot.solution(intArrayOf(1), 0))
+                .containsExactly(1)
+    }
+
+    @Test
+    internal fun `1 someNumber`() {
+        assertThat(arrayPivot.solution(intArrayOf(1), 43))
+                .containsExactly(1)
+    }
+
+    @Test
+    internal fun `2 0`() {
+        assertThat(arrayPivot.solution(intArrayOf(1, 2), 0))
+                .containsExactly(1, 2)
+    }
+
+    @Test
+    internal fun `2 1`() {
+        assertThat(arrayPivot.solution(intArrayOf(1, 2), 1))
+                .containsExactly(2, 1)
+    }
+
+    @Test
+    internal fun `2 2`() {
+        assertThat(arrayPivot.solution(intArrayOf(1, 2), 2))
+                .containsExactly(1, 2)
+    }
+
+    @Test
+    internal fun `3 1`() {
+        assertThat(arrayPivot.solution(intArrayOf(1, 2, 3), 1))
+                .containsExactly(3, 1, 2)
+    }
+
+    @Test
+    internal fun `3 2`() {
+        assertThat(arrayPivot.solution(intArrayOf(1, 2, 3), 2))
+                .containsExactly(2, 3, 1)
+    }
+
+    @Test
+    internal fun `3 3`() {
+        assertThat(arrayPivot.solution(intArrayOf(1, 2, 3), 3))
+                .containsExactly(1, 2, 3)
+    }
+
+    @Test
+    internal fun `3 4`() {
+        assertThat(arrayPivot.solution(intArrayOf(1, 2, 3), 4))
+                .containsExactly(3, 1, 2)
+
+
+        // TODO: try to get this down to 1 * (3-1) by shifting whole blocks
+        assertThat(arrayPivot.shiftsCount)
+                .isEqualTo(4 * 3)
+    }
+
 }
